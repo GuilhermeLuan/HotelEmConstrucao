f313b61f46dbc43ea483313ce9fac6f51cdb1b7b
Guilherme Luan <guilhermeluan@pm.me>
2023-11-21 09:24:25 -0300

    feat: funcao que valida a entrada do quarto

--- main.c
+++ main.c
@@ -11,6 +11,17 @@ int verificarQuartoOcupado(int quartoDesejado, int quartosOcupados[10]){
 
 }
 
+//Recebe o input do quarto desejado e verifica se a entrada do quarto é valida.
+int verificarQuartoValido(int quartoDesejado, int quartosVazios[10]){
+    for (int i = 0; i < 10; ++i) {
+        if(quartosVazios[i] == quartoDesejado){
+            return 1;
+        }
+    }
+    return 0;
+
+}
+
 int main(){
     //CODIGO ABAIXO É APENAS UM TESTE
     int quartosVazios[10] = {101, 102, 103, 104, 105, 106, 107, 108, 109, 110};
@@ -18,9 +29,16 @@ int main(){
     int quartoDesejado = 101;
 
     if(verificarQuartoOcupado(quartoDesejado, quartosOcupados)){
-        printf("Quarto Ocuapdo!");
+        printf("Quarto Ocuapdo!\n");
     } else {
-        printf("Quarto livre!");
+        printf("Quarto livre!\n");
+    }
+
+    if(verificarQuartoValido(quartoDesejado, quartosVazios)){
+        printf("Quarto eh valido\n");
+    } else {
+        printf("Quarto nao eh valido!\n");
+
     }
 
     return 0;
