9f906240b8e8ce59c8cd3380f94fd0429b307d29
Guilherme Luan <guilhermeluan@pm.me>
2023-11-27 20:33:38 -0300

    feat: funcao que remove elemento do array

---
+++ removerElemento.c
@@ -0,0 +1,15 @@
+void removerElemento(int array[], int *tamanho, int elemento) {
+    // Procurar o elemento no array
+    for (int i = 0; i < *tamanho; i++) {
+        if (array[i] == elemento) {
+            // Deslocar os elementos para preencher o espaço
+            for (int j = i; j < *tamanho - 1; j++) {
+                array[j] = array[j + 1];
+            }
+            // Atualizar o tamanho do array
+            (*tamanho)--;
+            // O elemento foi removido, podemos sair do loop
+            break;
+        }
+    }
+}
